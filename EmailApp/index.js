d960e0ef008f5edb2679ca5458828b0b4a94c3bf
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2022-07-02 17:23:45 +0530

    email registration with mailchimp

--- EmailApp/index.js
+++ EmailApp/index.js
@@ -3,13 +3,13 @@ const bp = require('body-parser');
 const rq = require("request");
 const https = require('https');
 // adding mailchimp
-// const client = require("mailchimp-marketing");
-// setting up mailchimp
-// client.setConfig({
-//   apiKey: "YOUR_API_KEY",
-//   server: "YOUR_SERVER_PREFIX",
-// });
+const client = require("@mailchimp/mailchimp_marketing");
 
+// setting up mailchimp
+client.setConfig({
+  apiKey: "d18fa409c8cc4e68baf620ef697f67f3-us17",
+  server: "us17",
+})
 
 const server = express();
 server.use(bp.urlencoded({
@@ -29,56 +29,50 @@ server.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+
 server.post("/", function(req, res) {
-  console.log(req.body);
   const firstName = req.body.fname;
   const lastName = req.body.lname;
-  const email = req.body.email;
+  var email = req.body.email;
   const birthdate = req.body.bDate;
 
+  console.log(firstName, lastName, email, birthdate);
   // console.log(firstName, lastName, email, birthdate);
   // here we are gping to create a object and assign value to paramters specified on mailchimp
 
-// this object is created according to mailchimp convection
-// every API HAS OWN input format
+  // this object is created according to mailchimp convection
+  // every API HAS OWN input format
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
 
-  const data = {
-    members:[
-      {
-      email_address:email,
-      status: "subscribed",
-
-      merge_fields: {
-        FNAME: firstName,
-        LNAME: lastName,
-        BIRTHDAY:birthdate,
-      }
-    }
-  ]
-};
-
-  // convert to data to object to json object and then we can send it to mailchimp
-  const dataJSON=JSON.stringify(data);
-  // console.log(dataJSON);
-
-  const url="https://us17.api.mailchimp.com/3.0/lists/ca59bc965a/members";
-
-  const options={
-    method:"POST",
-    auth:"salunkhegr1712@gmail.com:43f3fa12a77f6cecc446c71319e6526f-us17"
   }
-  // so we are sending the request to the mail chimp with https module
-  // this is syntax for https request
-  // https.request(url,options,function(responce){
-  //
-  // })
-  const request=https.request(url,options,function(responce){
-    responce.on("data",function(data){
-      console.log(JSON.parse(data));
-    })
-  });
-  request.write(dataJSON);
-  request.end();
-  res.send("hello world");
+
+
+  const run = async () => {
+    const response = await client.lists.addListMember("ca59bc965a", {
+      email_address: subscribingUser.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: subscribingUser.firstName,
+        LNAME: subscribingUser.lastName,
+      }
+
+    });
+
+    if (res.statusCode === 200) {
+
+      res.sendFile(__dirname + "/success.html");
+    }
+    else{
+      res.send("try again");
+    }
+  }
+
+  run();
+
+
+
 
 });
